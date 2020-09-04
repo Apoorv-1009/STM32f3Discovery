ee9e84c326358e52340fd0dbabd625c0a32ac419
Apoorv Thapliyal <57452076+Apoorv-1009@users.noreply.github.com>
2020-09-04 11:27:40 +0530

    Update Delay.c

--- Codes/Delay.c
+++ Codes/Delay.c
@@ -1,16 +1,15 @@
-//Code Generates a delay using Timer3 to Blink LD10(PE13)
+//Code Generates a delay in ms using Timer3 to Blink LD10(PE13)
 
 #include "stm32f303xc.h"
-#include "stm32f3xx.h"
 volatile int myTicks = 0;
 
 void Timer_Initialize()
 {
-	RCC->APB1ENR |= 1 << 3;   //Enable Timer3
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;   //Enable Timer3
 	TIM3->PSC = 72;   //Freq = 1Mhz
 	TIM3->DIER |= TIM_DIER_UIE;   //UIE: Update Interrupt Enable
 	TIM3->EGR |= TIM_EGR_UG;   //UG: Update Generation, Very Important
-	 NVIC_EnableIRQ_(TIM3_IRQn);
+	NVIC_EnableIRQ_(TIM3_IRQn);
 	
 }
 void TIM3_IRQ_Handler(void)
@@ -21,23 +20,30 @@ void TIM3_IRQ_Handler(void)
 void delay_ms(int del)
 {
 	//Enabling Counter to Start counting only when 
-	TIM3->CR1 |= TIM_CR1_CEN;   //Enable Timer3 Counting
+	TIM3->CR1 |= TIM_CR1_CEN;   //Start Timer3 Counting
 	myTicks = 0;
 	while(myTicks<(del*1000));
-	TIM3->CR1 &= ~(TIM_CR1_CEN);   //Disable Timer3 Counting
+	TIM3->CR1 &= ~(TIM_CR1_CEN);   //Stop Timer3 Counting
 }
 
 
 void GPIO_Initialize()
 {
-	RCC->AHBENR |= 1 << 21;   //Enable Clock for PortE
-	GPIOE->MODER |= 1 << 13;   //Enable PE13 as OUTPUT
+	RCC->AHBENR |= RCC_AHBENR_GPIOEEN;   //Enable Clock for PortE
+	
+	GPIOE->MODER |= GPIO_MODER_MODER13_0;   //Enable PE13 as OUTPUT
+	GPIOE->MODER |= ~(GPIO_MODER_MODER13_1);
+	
+	GPIOE->OTYPER &= ~(GPIO_OTYPER_OT_13);   //Output Push-Pull
+	GPIOE->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR13);   //High Speed
 }
 
 
 int main()
 {
-	int delay = 1000;   //Enter Delay here
+	GPIO_Initialize();
+	Timer_Initialize();
+	int delay = 1000;   //Enter Delay in ms here
 	while(1)
 	{
 		GPIOE->BSRR |= 1 << 13;
