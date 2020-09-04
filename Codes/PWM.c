fdf4b66fb856b3075b4181d32d3c7b8e2dbdbe5f
Apoorv Thapliyal <57452076+Apoorv-1009@users.noreply.github.com>
2020-09-04 11:29:44 +0530

    Update PWM.c

--- Codes/PWM.c
+++ Codes/PWM.c
@@ -6,28 +6,29 @@
 
 void Timer_Initialize()
 {
-	RCC->APB1ENR |= 1 << 1;   //Enable Timer3
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;   //Enable Timer3
+	TIM3->CCER |= TIM_CCER_CC1E;   //Enable Channel 1
+	TIM3->CR1 |= TIM_CR1_ARPE;   //Enable Auto Re-Load PreLoad (ARPE)
+	TIM3->CCMR1 |= TIM_CCMR1_OC1PE;  //Enable PreLoad
+	TIM3->CCER |= TIM_CCER_CC1E;   //Enable Timer3 as OUTPUT
+	TIM3->CCMR1 |= (TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2);   //PWM Mode 1
+	
 	TIM3->ARR = 65535;   //16 bit number
-	TIM3->CCMR1 |= (1 << 5 | 1 << 6);   //PWM Mode 1
-	TIM3->CCER |= 1 << 12;   //Enable Output
-	TIM3->CR1 |= 1 << 7;   //Enable Auto Re-Load PreLoad (ARPE)
 	TIM3->CCR1 = 0;
-	TIM3->CCMR1 |= 1 << 3;   //Enable PreLoad
-	TIM3->CCER |= 1 << 0;   //Output On PC6
-	TIM3->CR1 |= 1 << 0;   //Start Counting
+	TIM3->PSC |= 1;   //Prescaler
+	
+	TIM3->EGR |= TIM_EGR_UG;   //Update Registers
+	TIM3->CR1 |= TIM_CR1_CEN;   //Start Counting
 }
+
 
 void GPIO_Initialize()
 {
-	RCC->AHBENR |= 1 << 19;   //Enable Clock for Port C
+	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;   //Enable Clock for Port C
 	//Enable AF Mode for PC6:
-	GPIOC->MODER |= 1 << 13;
-	GPIOC->MODER &= 0 << 12;
-	GPIOC->OSPEEDR |= 1 << 12;   //Medium Speed
-	//Enable Pull Down:
-	GPIOC->PUPDR |= 1 << 13;   
-	GPIOC->PUPDR &= 0 << 12;
-	GPIOC->AFR[0] = 1 << 25;   
+	GPIOC->MODER &= ~(GPIO_MODER_MODER6_0);
+	GPIOC->MODER |= GPIO_MODER_MODER6_1;
+	GPIOC->AFR[0] |= GPIO_AFRL_AFRL2;   //AF2
 	
 }
 
@@ -39,12 +40,11 @@ int main()
 	while(1)
 	{
 	  i = 0;
-		while(i < 65535)
+		while(i < TIM3->ARR)
 		{
-			TIM4->CCR4 = i;
+			TIM3->CCR1 = i;
 			i++;
 		}
 	}
 	
 }
-
